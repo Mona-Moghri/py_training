1f8bc764a3ae6325fcf2e3d3e492e60993d55694
Mona Moghri <moghri.mona@gmail.com>
2024-11-15 21:52:49 +1000

    Updated code in file3.py with new functionality

---
+++ file3.py
@@ -0,0 +1,17 @@
+#price of a house is $1M, if buyer has good credit, put down 10%. otherwise put down 20%. print the down payment for good buyer
+#my code
+price='$1M'
+good_credit=False
+if good_credit:
+    print('they need to put down 10%')
+else:
+    print('they need to put down 20%')
+print('down payment')
+#correct answer
+"""price = 1000000
+good_credit= True
+if good_credit:
+    down_payment= 0.1*price
+else:
+    down_payment= 0.2*price
+print(f"down payment: ${down_payment}")"""
