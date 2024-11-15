c35e733fd8a6aee9a585a5f1d495bdcc0c7632cd
Mona Moghri <moghri.mona@gmail.com>
2024-11-15 19:25:06 +1000

    Update file1.py with new code

--- file1.py
+++ file1.py
@@ -1,1 +1,21 @@
-test
+#if statement practise
+weather= input('waht is the weather today? ')
+if weather == 'hot day':
+    print('Drink plenty of water')
+if weather == 'cold day':
+    print('Wear warm clothes')
+else:
+    print("It's lovely day")
+
+#write the previous one with boolean
+"""is_hot= False
+is_cold= False
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's a lovely day")
+print("enjoy your day")"""
