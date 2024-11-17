cba8603bc8fe5f438d36cbf5d5e49a8f722b9d24
Mona Moghri <moghri.mona@gmail.com>
2024-11-17 19:48:12 +1000

    added file7.py

---
+++ file7.py
@@ -0,0 +1,7 @@
+#assingment. calculate the total cost of the item in shopping card
+#mosh said it is easy but it makes me confuse at first so i need to check the solution
+price= [10, 20, 30]
+calculate=0   #keep track the result of each iteration
+for num in price:
+    calculate += num  #not big achievment but i solve it at first i write +=price  
+print(f'total: {calculate}')
